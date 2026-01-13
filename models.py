e0d741bce9909421139b869b9f61fb96078d3ed0
anshkunj <meghag2075@gmail.com>
2026-01-13 18:54:03 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 # models.py
 from pydantic import BaseModel
+from tying import List
 class Array_input(BaseModel) :
-	arr : list[int]
+	arr : List[int]
 	k : int
 class array_input(BaseModel) :
-	arr : list[int]
+	arr : List[int]
